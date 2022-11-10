5f16ea4de431c74eefb45d0249d18876a4982130
Jacek Karczmarczyk <jkarczm@gmail.com>
2022-11-11 00:47:10 +0100

    WIP

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,13 @@
 import hash from 'hash.js';
 import fs from 'node:fs';
 import type { OutputAsset, OutputPlugin } from 'rollup';
+import MagicString from 'magic-string';
 
 interface ImportmapPluginOptions {
   // External import maps are not supported yet
   external?: boolean;
+  base: string;
+  type?: 'inline' | 'external' | 'systemjs';
 }
 
 interface ImportMap {
@@ -33,11 +36,49 @@ function createImportMapScript (importMapAsset: OutputAsset, external?: boolean)
     : `<script type="importmap">${importMapSource}</script>`;
 }
 
-export default function ImportmapPlugin ({ external = false }: ImportmapPluginOptions = {}): OutputPlugin {
+export default function ImportmapPlugin ({
+                                           base,
+                                           type = 'inline',
+                                         }: ImportmapPluginOptions): OutputPlugin {
   const importMap: ImportMap = { imports: {} };
+  let contentToInclude = '';
+
+  function before (content: string): string {
+    return `if(!window.System){ ${content} }`;
+  }
+
+  function after (fileName: string): string {
+    return `System.import('${base}${fileName}')`;
+  }
 
   return {
     name: 'importmap-plugin',
+    renderStart () {
+      if (type !== 'systemjs') return;
+
+      contentToInclude = fs.readFileSync(require.resolve('systemjs/dist/s.js'), 'utf8');
+    },
+    augmentChunkHash (chunk) {
+      if (type === 'systemjs' && chunk.isEntry) {
+        return before(contentToInclude) + after(chunk.name);
+      }
+    },
+    async renderChunk (code, chunk, options) {
+      if (type !== 'systemjs' || !chunk.isEntry) {
+        return null;
+      }
+
+      const magicString = new MagicString(code, { filename: chunk.fileName });
+
+      magicString
+        .prepend(before(contentToInclude))
+        .append(after(chunk.fileName));
+
+      return {
+        code: magicString.toString(),
+        map: options.sourcemap ? magicString.generateMap() : null,
+      };
+    },
     generateBundle (config, bundle) {
       importMap.imports = {};
 
@@ -53,13 +94,17 @@ export default function ImportmapPlugin ({ external = false }: ImportmapPluginOp
       bundle.importMap = createImportMapAsset(importMap);
     },
     writeBundle (config, bundle) {
+      if (type === 'systemjs') {
+        return;
+      }
+
       const { dir = './dist', entryFileNames } = config;
       const entryFileName = `/${String(entryFileNames)}`;
-      const importMapCode = createImportMapScript(bundle.importMap as OutputAsset, external);
+      const importMapCode = createImportMapScript(bundle.importMap as OutputAsset, type === 'external');
       const indexPath = `${dir}/index.html`;
       const indexHtml = fs.readFileSync(indexPath, 'utf-8');
 
-      if (!external) {
+      if (type !== 'external') {
         fs.unlinkSync(`${dir}/${bundle.importMap.fileName}`);
       }
 
